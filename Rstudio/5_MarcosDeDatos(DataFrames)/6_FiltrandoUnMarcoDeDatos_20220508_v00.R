dcb4b5e2931f47a8931c69153fd872a4edd6c0be
Roberto Velasquez <77901176+Rescata1995@users.noreply.github.com>
2022-05-13 23:53:18 -0500

    Update 6_FiltrandoUnMarcoDeDatos_20220508_v00.R

--- Rstudio/5_MarcosDeDatos(DataFrames)/6_FiltrandoUnMarcoDeDatos_20220508_v00.R
+++ Rstudio/5_MarcosDeDatos(DataFrames)/6_FiltrandoUnMarcoDeDatos_20220508_v00.R
@@ -1,22 +1,22 @@
 
 "
-Sobre cómo filtrar Marcos de datos
+Sobre cÃ³mo filtrar Marcos de datos
 
-Si bien recuerdas, el concepto de 'Filtros' no sólo se usa en R, 
-también se usa en Hojas de cálculo y en SQL (Cláusula Where) y 
-se valen exactamente de la misma estructura y dinámica lógica.
+Si bien recuerdas, el concepto de 'Filtros' no sÃ³lo se usa en R, 
+tambiÃ©n se usa en Hojas de cÃ¡lculo y en SQL (ClÃ¡usula Where) y 
+se valen exactamente de la misma estructura y dinÃ¡mica lÃ³gica.
 Los filtros se dedican especialmente a filtrar registros (filas).
 
-Un filtro es usado cuando tú sólo estás interesado en ver datos
-que cumplen un criterio en específico, ocultando el resto de los 
-datos. La intención de esta clase es mostrarte cómo se filtran
+Un filtro es usado cuando tÃº sÃ³lo estÃ¡s interesado en ver datos
+que cumplen un criterio en especÃ­fico, ocultando el resto de los 
+datos. La intenciÃ³n de esta clase es mostrarte cÃ³mo se filtran
 datos desde R. 
 
 "
 
 "
 Revisemos nuestros datos primeramente. Recuerde que estamos 
-trabajando con el dataset que se asenta en la varaible 'Data'
+trabajando con el dataset que se asenta en la variable 'Data'
 
 "
 
@@ -29,10 +29,10 @@ Ejercicios de Filtrado.
 A. Queremos ver los registros con una 'Penetracion.Internet' menor 
 al 2%. 
 
-aclaración: Si se percata, se estarán filtrando registros; pues,
-aún te interesa ver los datos de todas las columnas, es decir,
+aclaraciÃ³n: Si se percata, se estarÃ¡n filtrando registros; pues,
+aÃºn te interesa ver los datos de todas las columnas, es decir,
 un registro entero pero si, y solo si, estos registros cumplen 
-con la condición de que su 'Penetracion.Internet' sea menor a 2%.
+con la condiciÃ³n de que su 'Penetracion.Internet' sea menor a 2%.
 
 "
 
@@ -41,39 +41,39 @@ con la condición de que su 'Penetracion.Internet' sea menor a 2%.
 
 Para hacer un filtrado en R debemos recurrir a la forma de acceder 
 a todos los elementos de una columna, para un Dataframe, por medio 
-del símbolo $; esto es así porque el filtrado o el criterio que se
-debe cumplir se evalúa sobre una columna o campo en específico 
-(al menos en principio); entonces, luego, sólo se filtrarían los 
-registros que cumplan con ese criterio o condición en cuestión.
+del sÃ­mbolo $; esto es asÃ­ porque el filtrado o el criterio que se
+debe cumplir se evalÃºa sobre una columna o campo en especÃ­fico 
+(al menos en principio); entonces, luego, sÃ³lo se filtrarÃ­an los 
+registros que cumplan con ese criterio o condiciÃ³n en cuestiÃ³n.
 
 Posteriormente, luego de llamar a la columna por medio de $, se 
-pasa el criterio o la condición en concreto. 
+pasa el criterio o la condiciÃ³n en concreto. 
 
 Entonces, lo ideal es que asigne el filtrado a una variable. 
 Para abordar nuestro primer ejercicio a nuestra variable la 
 llamaremos 'filtro1'.
 
-Veamos lo anterior con la resolución del 1er ejercicio.
+Veamos lo anterior con la resoluciÃ³n del 1er ejercicio.
 
 "
 
 filtro1 <- Data$Penetracion.Internet < 2
 
 "
-Si usted corre el anterior código, lo que se hará es probar la 
-validez lógica del valor contenido en cada celda de 
+Si usted corre el anterior cÃ³digo, lo que se harÃ¡ es probar la 
+validez lÃ³gica del valor contenido en cada celda de 
 'Penetracion.Internet' para cada registro, arrojando resultados 
-de la evaluación lógica por medio de valores booleanos; es decir, 
+de la evaluaciÃ³n lÃ³gica por medio de valores booleanos; es decir, 
 mostrando por pantalla, para cada celda, cumpla o no, los valores 
 'TRUE' o 'FALSE', respectivamente.
 
-Entonces, si quisiera filtrar sólo los registros (filas) que 
-cumplan la condición 'Data$Penetracion.Internet < 2' debe llamar su 
+Entonces, si quisiera filtrar sÃ³lo los registros (filas) que 
+cumplan la condiciÃ³n 'Data$Penetracion.Internet < 2' debe llamar su 
 dataframe evaluado (Data) y, entre corchetes[], pasar la variable de 
 su filtro (filtro1)... 
 
-Importante: La variable de su filtro se pasa en la posición de las 
-filas, no en la de columnas, la posición de columnas la deja en 
+Importante: La variable de su filtro se pasa en la posiciÃ³n de las 
+filas, no en la de columnas, la posiciÃ³n de columnas la deja en 
 blanco. Observe:
 
 "
@@ -82,7 +82,7 @@ Data[filtro1,]
 
 "
 Ahora se muestra por pantalla solamente los registros de 'Data' que 
-cumplan con la condición o criterio 'Data$Penetracion.Internet < 2'.
+cumplan con la condiciÃ³n o criterio 'Data$Penetracion.Internet < 2'.
 
 "
 
@@ -101,7 +101,7 @@ Data[filtro2,]
 
 "
 Extra. Filtros con '==' (el registro debe tener una columna con
-un valor definido igual a la condición pasada con '==')
+un valor definido igual a la condiciÃ³n pasada con '==')
 
 C. Queremos ver los registros con un 'Grupo.Ingresos' que sea 
 igual a un 'Ingreso alto'.
@@ -117,7 +117,7 @@ Data[filtro3,]
 
 1.1 Mecanismo de Filtrado: filtrado de filas directo.
 
-O usted también podría pasar el filtro directamente, si hacer uso
+O usted tambiÃ©n podrÃ­a pasar el filtro directamente, si hacer uso
 de una variable. Observe.
 
 "
@@ -125,25 +125,25 @@ de una variable. Observe.
 Data[Data$Tasa.Natalidad > 40,]
 
 "
-Insight: Por cada 1000 habitantes, los países arrojados va a tener 
-al menos 40 nacimientos por año
+Insight: Por cada 1000 habitantes, los paÃ­ses arrojados va a tener 
+al menos 40 nacimientos por aÃ±o
 
 "
 
 
 "
-2. Filtrado múltiple.
+2. Filtrado mÃºltiple.
 
-2.1 filtrado múltiple con & (todas las condiciones se deben cumplir).
+2.1 filtrado mÃºltiple con & (todas las condiciones se deben cumplir).
 
-Para hacer un filtrado múltiple, donde se tengan que cumplir todas 
-las condiciones o criterios pasados, es sencillo: sólo tiene que 
-agregar cuantos filtros desee por medio del símbolo & (sólo uno por 
+Para hacer un filtrado mÃºltiple, donde se tengan que cumplir todas 
+las condiciones o criterios pasados, es sencillo: sÃ³lo tiene que 
+agregar cuantos filtros desee por medio del sÃ­mbolo & (sÃ³lo uno por 
 filtro).
 
-Veamos. Supongamos que queremos solamente aquellos países con una 
+Veamos. Supongamos que queremos solamente aquellos paÃ­ses con una 
 tasa de natalidad mayor a 40 (Data$Tasa.Natalidad > 40) y (&) 
-una penetración de internet menor que 2 (Data$Penetracion.Internet 
+una penetraciÃ³n de internet menor que 2 (Data$Penetracion.Internet 
 < 2).
 
 "
@@ -151,25 +151,25 @@ una penetración de internet menor que 2 (Data$Penetracion.Internet
 Data[Data$Tasa.Natalidad > 40 & Data$Penetracion.Internet < 2,]
 
 "
-Entonces, sólo se muestran aquellos registros del dataframe 'Data'
-que cumpla con las condiciones pasadas; en este caso, sería:
+Entonces, sÃ³lo se muestran aquellos registros del dataframe 'Data'
+que cumpla con las condiciones pasadas; en este caso, serÃ­a:
 'Data$Tasa.Natalidad > 40' & 'Data$Penetracion.Internet'.
 
 "
 
 
 "
-2.2 filtrado múltiple con | (sólo es necesario que se cumpla una 
+2.2 filtrado mÃºltiple con | (sÃ³lo es necesario que se cumpla una 
 de todas las condiciones pasadas).
 
-Para hacer un filtrado múltiple, donde sólo es necesario que se  
-cumpla una condición o criterio, de todos los pasados, es sencillo: 
-sólo tiene que agregar cuantos filtros desee por medio del símbolo 
-| (sólo uno por filtro).
+Para hacer un filtrado mÃºltiple, donde sÃ³lo es necesario que se  
+cumpla una condiciÃ³n o criterio, de todos los pasados, es sencillo: 
+sÃ³lo tiene que agregar cuantos filtros desee por medio del sÃ­mbolo 
+| (sÃ³lo uno por filtro).
 
-Veamos. Supongamos que queremos ver aquellos países con una 
-tasa de natalidad mayor a 40 (Data$Tasa.Natalidad > 40) ó (|) con
-una penetración de internet menor que 2 (Data$Penetracion.Internet 
+Veamos. Supongamos que queremos ver aquellos paÃ­ses con una 
+tasa de natalidad mayor a 40 (Data$Tasa.Natalidad > 40) Ã³ (|) con
+una penetraciÃ³n de internet menor que 2 (Data$Penetracion.Internet 
 < 2). 
 
 "
@@ -179,6 +179,6 @@ Data[Data$Tasa.Natalidad > 40 | Data$Penetracion.Internet < 2,]
 "
 Entonces, se muestran aquellos registros del dataframe 'Data'
 que cumpla con alguna de las condiciones pasadas; en este caso,
-sería: 'Data$Tasa.Natalidad > 40' ó 'Data$Penetracion.Internet'.
+serÃ­a: 'Data$Tasa.Natalidad > 40' Ã³ 'Data$Penetracion.Internet'.
 
 "
