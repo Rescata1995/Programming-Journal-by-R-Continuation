9e127ed565804425e837b4ffa4bb5b27b7275f0d
Roberto Velasquez <robertovelasquezdean@gmail.com>
2022-10-01 18:02:53 -0500

    Redondeo & Decimales en R

---
+++ R & Rstudio/3_FundamentosDeR/EXTRA_Redondeo&DecimalesEnR/2_Practica.R
@@ -0,0 +1,32 @@
+
+# Redondeo & Decimales en R
+
+print(sqrt(2),10)
+round(sqrt(2), 3)
+floor(sqrt(2))
+ceiling(sqrt(2))
+trunc(sqrt(2))
+
+sqrt(2)^2-2
+
+round(sqrt(2), 4)^2
+
+2^50
+print(2^50, 15)
+print(2^50, 2)
+print(pi, 22)
+#3.141592653589793115998
+#3.141592653589793238462
+
+round(1.25,1)
+round(1.35,1)
+round(sqrt(2),0)
+round(sqrt(2))
+
+round(digits = 5, sqrt(2))
+round(5, sqrt(2))
+
+
+floor(-3.45)
+ceiling(-3.45)
+trunc(-3.45)
