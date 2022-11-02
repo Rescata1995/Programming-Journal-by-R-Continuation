e02c2f003d00ef48c0fe2dec6247945e2482aaa9
Roberto Velasquez <77901176+Rescata1995@users.noreply.github.com>
2022-11-02 08:29:24 -0500

    Update 3_Factores&ExploracionDeDatos_20220516_v00.R

--- R & Rstudio/6_VisualizacionesAvanzadasConGGPlot2/3_Factores&ExploracionDeDatos_20220516_v00.R
+++ R & Rstudio/6_VisualizacionesAvanzadasConGGPlot2/3_Factores&ExploracionDeDatos_20220516_v00.R
@@ -1,6 +1,6 @@
 
 "
-Sobre los tipos de dato 'Factor' & La exploración de datos en general
+Sobre los tipos de dato 'Factor' & La exploraciÃ³n de datos en general
 
 "
 
@@ -10,25 +10,25 @@ Datos <- read.csv(file.choose())
 head(Datos)
 
 "
-Extra: Tratamiento ideal de archivos con 'headlines' que usan símbolos 
+Extra: Tratamiento ideal de archivos con 'headlines' que usan sÃ­mbolos 
        especiales y que, posteriormente, son importados en R.
        
 Hay veces que los campos de un archivo importado en R vienen con nombres
-o etiquetas que ocupan, dentro sí, simbologías. Por ej: %, $, o incluso 
-paréntesis; a R se le dificulta interpretar este tipo de caracteres, 
+o etiquetas que ocupan, dentro sÃ­, simbologÃ­as. Por ej: %, $, o incluso 
+parÃ©ntesis; a R se le dificulta interpretar este tipo de caracteres, 
 por lo cual, los reemplaza con simples puntos (.)
 
-Lo ideal en este tipo de casos es renombrar las columnas con la función
+Lo ideal en este tipo de casos es renombrar las columnas con la funciÃ³n
 'colnames()'. Probamos esto con el dataframe 'Ratings+Peliculas', el
 cual ya fue importado y almacenado en R dentro de la variable 'Datos'.
 
-Tip: Evite el uso de tildes también en el nombramiento de sus campos.
+Tip: Evite el uso de tildes tambiÃ©n en el nombramiento de sus campos.
 
 
 "
 
 colnames(Datos) <- c("Peliculas", "Genero", "Rating_Criticos", "Rating_Audiencia",
-                     "Presupuesto_Millones", "Año")
+                     "Presupuesto_Millones", "AÃ±o")
 
 "
 Echemos un vistazo de nuevo sobre los primeros 5 registros de 'Datos'
@@ -41,8 +41,8 @@ head(Datos)
 
 "
 Cambio efectuado con exito. Revisemos ahora la estructura de 'Datos'
-para identificar cuáles son nuestros campos con tipo de dato 'factor';
-es decir, cuáles son nuestras variables que resultan ser categóricas.
+para identificar cuÃ¡les son nuestros campos con tipo de dato 'factor';
+es decir, cuÃ¡les son nuestras variables que resultan ser categÃ³ricas.
 
 "
 
@@ -50,29 +50,29 @@ str(Datos)
 
 
 "
-1. Importancia de la función 'as.factor' & de todas las 'as. ...'
+1. Importancia de la funciÃ³n 'as.factor' & de todas las 'as. ...'
 
-Cualquier función de tipo 'as. ...', una función propia del proceso 
+Cualquier funciÃ³n de tipo 'as. ...', una funciÃ³n propia del proceso 
 de 'Data Transformation' o, en general, del 'Data cleaning' tiene la 
 facultad de convertir y cambiar el tipo de dato de una columna pasada
 que, se cree, no ha sido interpretada correctamente por R con su tipo
 de dato original: generando problemas de 'Data inconsistency'.
 
-Lo anterior nos está sucediendo ahora mismo con los campos 'Peliculas' 
-& 'Genero', Rstudio los está reconociendo como 'chr' cuando no es así.
+Lo anterior nos estÃ¡ sucediendo ahora mismo con los campos 'Peliculas' 
+& 'Genero', Rstudio los estÃ¡ reconociendo como 'chr' cuando no es asÃ­.
 Los campos 'Peliculas' & 'Genero' deben ser reconocidos como 'factor'. 
 
 Hay varias versiones de Rstudio que muestran este tipo de dato como 'chr'; 
 es decir, de tipo 'character'. Si Rstudio no reconoce el tipo de dato 
-'Factor', en consecuencia, no se mostrarían los 'levels' de dichos campos 
-al tratar de visualizarlos con la función 'str()' o con la función 
+'Factor', en consecuencia, no se mostrarÃ­an los 'levels' de dichos campos 
+al tratar de visualizarlos con la funciÃ³n 'str()' o con la funciÃ³n 
 'levels()' directamente. Para solucionar el anterior problema lo que 
-se podría hacer es correr directamente la función 'as.factor()' sobre los 
-campos tipo 'chr' que desee convertir de la tabla de su interés, en este
-caso, de la tabla 'Datos'; y, posteriormente, esa conversión debe ser 
-asignada de forma directa sobre los mismos campos de interés por medio de 
-un puntero; es decir, en este caso puntual, le asignamos la conversión 
-tanto a 'Datos$Peliculas' como a 'Datos$Genero'. Tal que así:
+se podrÃ­a hacer es correr directamente la funciÃ³n 'as.factor()' sobre los 
+campos tipo 'chr' que desee convertir de la tabla de su interÃ©s, en este
+caso, de la tabla 'Datos'; y, posteriormente, esa conversiÃ³n debe ser 
+asignada de forma directa sobre los mismos campos de interÃ©s por medio de 
+un puntero; es decir, en este caso puntual, le asignamos la conversiÃ³n 
+tanto a 'Datos$Peliculas' como a 'Datos$Genero'. Tal que asÃ­:
 
 "
 
@@ -83,10 +83,10 @@ Datos$Genero <- as.factor(Datos$Genero)
 "
 Ahora, tanto 'Peliculas' como 'Genero' conservan el tipo de dato 'factor'.
 
-Extra: Exploración de datos, muy importante para evaluar consistencia.
+Extra: ExploraciÃ³n de datos, muy importante para evaluar consistencia.
 
-Comprobemos que 'Peliculas' & 'Genero' ahora sí conservan un tipo de dato
-'factor' con la función 'str()' o con la función 'levels()' directamente.
+Comprobemos que 'Peliculas' & 'Genero' ahora sÃ­ conservan un tipo de dato
+'factor' con la funciÃ³n 'str()' o con la funciÃ³n 'levels()' directamente.
 
 "
 
@@ -97,34 +97,34 @@ levels(Datos$Genero)
 
 "
 
-Recuerde: Los 'levels', haciendo un símil con SQL, sería el equivalente a 
+Recuerde: Los 'levels', haciendo un sÃ­mil con SQL, serÃ­a el equivalente a 
 extraer los datos de un campo por medio de una sentencia de tipo 'DISTINCT'.
 
 Adelanto: Es ideal, en R, que los campos que guardan fechas sean tratados 
-también como campos de tipo 'Factor'. En muchas ocasiones R, por defecto, 
-interpreta a las fechas como valores numéricos, por lo que, probablemente, 
-le toque de nuevo hacer uso de la función 'as.factor' para corregir esta
+tambiÃ©n como campos de tipo 'Factor'. En muchas ocasiones R, por defecto, 
+interpreta a las fechas como valores numÃ©ricos, por lo que, probablemente, 
+le toque de nuevo hacer uso de la funciÃ³n 'as.factor' para corregir esta
 posible inconsistencia y convertir las fechas en un campo de tipo 'factor'.
 
-Más adelante verá que nuestro campo 'Año' está siendo tratado como campo
-numérico por la forma en como la función 'summary' hace un resumen del 
-mismo; esto no es lo deseado, nos tocará convertir sus datos a datos tipo 
-'factor' para poder filtrar representaciones visuales según año de estudio;
-es decir, para poder crear facetas según un año de estudio evaluado.
+MÃ¡s adelante verÃ¡ que nuestro campo 'AÃ±o' estÃ¡ siendo tratado como campo
+numÃ©rico por la forma en como la funciÃ³n 'summary' hace un resumen del 
+mismo; esto no es lo deseado, nos tocarÃ¡ convertir sus datos a datos tipo 
+'factor' para poder filtrar representaciones visuales segÃºn aÃ±o de estudio;
+es decir, para poder crear facetas segÃºn un aÃ±o de estudio evaluado.
 
 "
 
 
 "
-2. Revisión de la Estructura & tipo de dato de cada campo por medio de la 
-función 'summary'
+2. RevisiÃ³n de la Estructura & tipo de dato de cada campo por medio de la 
+funciÃ³n 'summary'
 
 Se pretende visualizar algunas caracteristicas de los tipos de dato sobre 
 cada campo del dataframe 'Ratings+Peliculas', almacenado en la variable 'Datos'. 
-Tenga en cuenta que la función 'summary' hará en resumen de cada columna tomando 
+Tenga en cuenta que la funciÃ³n 'summary' harÃ¡ un resumen de cada columna tomando 
 en cuenta el tipo de dato que conserve la misma; es decir, no es el mismo
-tipo de resumen que se ve en una columna con tipo de dato 'numérico' al 
-resumen que se ve en una columna con tipo de dato 'factor' o categórico. 
+tipo de resumen que se ve en una columna con tipo de dato 'numÃ©rico' al 
+resumen que se ve en una columna con tipo de dato 'factor' o categÃ³rico. 
 
 "
 
@@ -133,26 +133,26 @@ summary(Datos)
 
 "
 2.1 Campos que almacenan fechas como campos con tipo de dato 'Factor' 
-(o categóricos) para poder crear facetas con ellos.
+(o categÃ³ricos) para poder crear facetas con ellos.
 
-Recuerde, son los campos categóricos los que nos permiten generar
-facetas en la visualización de nuestros datos. Entonces, tenemos que
-el campo 'Año' es un campo que almacena fechas; es decir, debe ser 
-un campo categórico.
+Recuerde, son los campos categÃ³ricos los que nos permiten generar
+facetas en la visualizaciÃ³n de nuestros datos. Entonces, tenemos que
+el campo 'AÃ±o' es un campo que almacena fechas; es decir, debe ser 
+un campo categÃ³rico.
 
-Puntualmente, en campos de tipo numérico lo que se ven son cálculos 
-agregados; y, de hecho, observe que el campo 'Año' está siendo tratado, 
-justamente, como campo de tipo numérico. En efecto, el tipo de dato de 
-'Año' es 'Int' (son enteros) y esto no es lo que se busca. Lo que se busca 
-es que el campo 'Año' sea tratado como una categoría, un campo 'factor',
-para poder distinguir la información de un año con la información de otro
+Puntualmente, en campos de tipo numÃ©rico lo que se ven son cÃ¡lculos 
+agregados; y, de hecho, observe que el campo 'AÃ±o' estÃ¡ siendo tratado, 
+justamente, como campo de tipo numÃ©rico. En efecto, el tipo de dato de 
+'AÃ±o' es 'Int' (son enteros) y esto no es lo que se busca. Lo que se busca 
+es que el campo 'AÃ±o' sea tratado como una categorÃ­a, un campo 'factor',
+para poder distinguir la informaciÃ³n de un aÃ±o con la informaciÃ³n de otro
 por medio de representaciones visuales separadas; es decir, poder generar 
-'facetas' (una de las 7 capas de la gramática de gráficos).
+'facetas' (una de las 7 capas de la gramÃ¡tica de grÃ¡ficos).
 
-Procedamos a cambiar el tipo de dato de 'Año' a 'Factor'.
+Procedamos a cambiar el tipo de dato de 'AÃ±o' a 'Factor'.
 
 "
 
-Datos$Año <- as.factor(Datos$Año)
+Datos$AÃ±o <- as.factor(Datos$AÃ±o)
 
 str(Datos)
